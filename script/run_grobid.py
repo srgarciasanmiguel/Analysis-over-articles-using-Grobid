200a67265e92cc1087362e0006fd586101cb7f46
RODRÍGUEZ GARCÍA SAN MIGUEL, SANTIAGO <s.rgarciasanmiguel@alumnos.upm.es>
2026-03-09 15:29:19 +0100

    primera iteración de los scripts hecha, requirements modificados

--- script/run_grobid.py
+++ script/run_grobid.py
@@ -3,17 +3,17 @@ import os
 
 GROBID_URL = "http://localhost:8070/api/processFulltextDocument"
 
-pdf_folder = "../data/pdfs"
-xml_folder = "../data/xml"
+pdf_dir = "../data/pdfs"
+xml_dir = "../data/xml"
 
-os.makedirs(xml_folder, exist_ok=True)
+os.makedirs(xml_dir, exist_ok=True)
 
-for pdf in os.listdir(pdf_folder):
-    with open(os.path.join(pdf_folder, pdf), 'rb') as f:
+for pdf in os.listdir(pdf_dir):
+    with open(os.path.join(pdf_dir, pdf), 'rb') as f:
         files = {'input': f}
         r = requests.post(GROBID_URL, files=files)
 
-    output_file = os.path.join(xml_folder, pdf.replace(".pdf",".xml"))
+    output_file = os.path.join(xml_dir, pdf.replace(".pdf",".xml"))
 
     with open(output_file, "w", encoding="utf-8") as out:
         out.write(r.text)
